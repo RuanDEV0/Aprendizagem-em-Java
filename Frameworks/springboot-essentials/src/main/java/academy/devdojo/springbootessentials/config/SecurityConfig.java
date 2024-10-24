aada263c19c0a8a6add1f8544c908280d25855f2
Ruan <oliveiraruan2018@gmail.com>
2024-10-24 12:10:17 -0400

    successufull spring security part 1

--- Frameworks/springboot-essentials/src/main/java/academy/devdojo/springbootessentials/config/SecurityConfig.java
+++ Frameworks/springboot-essentials/src/main/java/academy/devdojo/springbootessentials/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,28 +12,37 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 @EnableWebSecurity
 @Log4j2
 public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-//		return http.build();
-//    }
-    @Bean
-    public UserDetailsService userDetailsService(){
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.csrf().disable() // Caso você não esteja usando CSRF
+				.authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+				.httpBasic(httpSecurityHttpBasicConfigurer -> {}); // Habilita o login com formulário, se necessário
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
 		PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		UserDetails user = User.withDefaultPasswordEncoder()
+
+		UserDetails user = User.builder()
 				.username("william")
-				.password("academy")
+				.password(passwordEncoder.encode("academy")) // Usa o PasswordEncoder
 				.roles("USER", "ADMIN")
 				.build();
-		UserDetails userTwo = User.withDefaultPasswordEncoder()
+
+		UserDetails userTwo = User.builder()
 				.username("devdojo")
-				.password("academy")
+				.password(passwordEncoder.encode("academy")) // Usa o PasswordEncoder
 				.roles("USER")
 				.build();
+
 		return new InMemoryUserDetailsManager(user, userTwo);
-    }
+	}
 }
